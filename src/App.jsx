47fa2f2d0ffac81525009e368f417055975a5c22
Bhakti Jadhav <bjadhav@agivant.com>
2025-05-03 22:01:17 +0530

    vercel analytics deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./component/Navbar/Navbar"; // <-- Make sure this is imported
 import Hero from "./component/Hero/Hero";
+import { Analytics } from '@vercel/analytics/react';
 import Banner from "./component/Banner/Banner";
 import Services from "./component/Services/Services";
 import Subscribe from "./component/Subscribe/Subscribe";
@@ -8,9 +9,11 @@ import Banner2 from "./component/Banner/Banner2";
 import Team1 from "./component/Team/Team1";
 import Team2 from "./component/Team/Team2";
 
+
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
+      <Analytics />
       <Navbar /> {/* Add your navbar at the top */}
       <div className="pt-24"> {/* Add top padding to avoid navbar overlap */}
         <Hero />
